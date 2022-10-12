169ac24fc3e46c1d7c314932ef93c87900bdca8d
joseldos <36111012+joseldos@users.noreply.github.com>
2022-10-12 11:41:34 +0200

    Create ejercicio-02.js

---
+++ ejercicio-02.js
@@ -0,0 +1,13 @@
+let nombre = "José Luis";
+let apellido = "dos Ramos";
+let nombre_completo = nombre + " " + apellido;
+let edad = 37;
+let desarollo = true;
+let fecha = new Date("april 24 1985 07:00:00")
+let libro_favorito = {
+    titulo:"La ciudad de las bestias",
+    autor:"Isabel Allende",
+    fecha:2002,
+    url: "https://www.amazon.es/ciudad-Bestias-Memorias-%C3%81guila-Jaguar/dp/8497935691/ref=sr_1_1?keywords=la+ciudad+de+las+bestias&qid=1665567543&qu=eyJxc2MiOiIyLjAxIiwicXNhIjoiMS4zNyIsInFzcCI6IjEuMjQifQ%3D%3D&sprefix=la+ciudad+de+las+%2Caps%2C96&sr=8-1",
+}
+
