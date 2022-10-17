815527b1e6d8b4bcc74d03b771e463fd53f245ea
joseldos <36111012+joseldos@users.noreply.github.com>
2022-10-17 12:19:09 +0200

    Update ejercicio-04.js

--- ejercicio-04.js
+++ ejercicio-04.js
@@ -9,4 +9,12 @@ let ultimaLetra = apellido.charAt(apellido.length-1);
 let estudianteSinEspacio = estudiante.trim();
 let contenido = estudiante.includes(nombre);
 
+console.log(estudiante);
+console.log(estudianteMayus);
+console.log(estudianteMinus);
+console.log(numero);
+console.log(primeraLetra);
+console.log(ultimaLetra);
+console.log(estudianteSinEspacio);
+console.log(contenido);
 
