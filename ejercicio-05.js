07a468821740ae4dcffd1794bd386c43510e5254
joseldos <36111012+joseldos@users.noreply.github.com>
2022-10-17 18:11:27 +0200

    Create ejercicio-05.js

---
+++ ejercicio-05.js
@@ -0,0 +1,10 @@
+let altura = 168;
+let alt_mts = 1.68;
+let peso_kg = 115;
+let alt_rd = Math.ceil(alt_mts);
+let alt_rd_dw = Math.floor(alt_mts);
+let var_max = Number.MAX_VALUE + 1;
+
+console.log(alt_rd);
+console.log(alt_rd_dw);
+console.log(var_max);
