13de9310751eb5eee117f92ac6ac078d689f49f4
joseldos <36111012+joseldos@users.noreply.github.com>
2022-10-17 12:17:04 +0200

    Create ejercicio-04.js

---
+++ ejercicio-04.js
@@ -0,0 +1,12 @@
+let nombre = "Jóse Luis";
+let apellido = "dos Ramos";
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let numero = estudiante.length;
+let primeraLetra = nombre.charAt(0);
+let ultimaLetra = apellido.charAt(apellido.length-1);
+let estudianteSinEspacio = estudiante.trim();
+let contenido = estudiante.includes(nombre);
+
+
