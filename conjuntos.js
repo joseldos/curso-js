521813e95c7e09f75623d3a5e999747ff516e309
joseldos <36111012+joseldos@users.noreply.github.com>
2022-10-21 16:48:43 +0200

    Create conjuntos.js

---
+++ conjuntos.js
@@ -0,0 +1,10 @@
+const miSet = new Set(
+    ["Mama", "Papa", "Abuelo", "Abuela", "Perro 1", "Perro 2", "tio", "primo", "yo"]
+)
+
+miSet.add("yo")
+
+console.log(miSet)
+
+miSet.add("JavaScript")
+console.log(miSet)
